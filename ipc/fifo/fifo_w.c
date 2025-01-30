333fc5754efb021d72c218d5d4ba2c2cd33663e2
zb <1662308219@qq.com>
2025-01-30 21:47:52 +0800

    daily

---
+++ ipc/fifo/fifo_w.c
@@ -0,0 +1,31 @@
+#include<unistd.h>
+#include<string.h>
+#include<stdlib.h>
+int main(int argc,char*argv[])
+{
+    int fd,i;
+    char buf[4096];
+    if(argc<2)
+    {
+        printf("enter like this:./a.out filename\n");
+        return -1;
+    }
+
+    fd=open(argv[1],O_WRONLY);
+    if(fd<0)
+    {
+        perror("open");
+        exit(1);
+    }
+
+    i=0;
+    while(1)
+    {
+        sprintf(buf,"hello %d\n",i++);
+        write(fd,buf,sizeof(buf));
+        sleep(1);
+    }
+    close(fd);
+
+    return 0;
+}
