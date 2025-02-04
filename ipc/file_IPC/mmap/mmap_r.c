034bdc4bfeaf7c658be7ec7821bc472eb70da54f
zb <1662308219@qq.com>
2025-02-04 16:03:40 +0800

    daily

--- ipc/file_IPC/mmap/mmap_r.c
+++ ipc/file_IPC/mmap/mmap_r.c
@@ -14,11 +14,10 @@ struct student
 };
 int main()
 {
-    struct student stu={1,"xiaoming",16};
-    int fd=open("test_map",O_RDWR|O_CREAT|O_TRUNC,0644);
-    ftruncate(fd,sizeof(struct student));
+    struct student stu;
+    int fd=open("test_map",O_RDONLY);
 
-    struct student *p=mmap(NULL,sizeof(stu),PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+    struct student *p=mmap(NULL,sizeof(stu),PROT_READ,MAP_SHARED,fd,0);
     if(p==MAP_FAILED)
     {
         perror("mmap");
@@ -28,10 +27,10 @@ int main()
 
     while(1)
     {
-        memcpy(p,&stu,sizeof(stu));
-        stu.id++;
+        printf("id=%d,name=%s,age=%d\n",p->id,p->name,p->age);
         sleep(2);
     }
+
     munmap(p,sizeof(stu));
     return 0;
 } 
